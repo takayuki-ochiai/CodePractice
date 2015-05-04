54920d691730735c0db9f7161861dd49aefb8512
落合隆行 <TakayukiOchiai@ochiaitakayuki-no-MacBook-Pro.local>
2015-05-04 13:18:00 +0900

    動的メソッドの定義 リファクタリング終了

--- RubyPractice/Refactoring/dynamic_method_definition.rb
+++ RubyPractice/Refactoring/dynamic_method_definition.rb
@@ -1,11 +1,43 @@
-def failure
-  self.state = :failure
+# def failure
+#   self.state = :failure
+# end
+
+# def error
+#   self.state = :error
+# end
+
+# def success
+#   self.state = :success
+# end
+
+%i(failure error success).each do |method_name|
+  define_method method_name do
+    self.state = method_name
+  end
 end
 
-def error
-  self.state = :error
+#こんなメソッドを定義する方法もある
+class Class
+  def def_each(*method_names, &block)
+    method_names.each do |method_name|
+      define_method method_name do
+        instance_exec method_name, &block
+      end
+    end
+  end
 end
 
-def success
-  self.state = :success
+#気づき：　たしかに重複は無くなるけど、オープンクラスしてたり考えることが増えるのであまり好みの書き方じゃない。
+#クラスマクロを作る方が好きだなあ
+
+class Post
+  def self.states(*args)
+    args.each do |arg|
+      define_method arg do
+        self.state = arg
+      end
+    end
+  end
+
+  states :failure, :error, :success
 end
