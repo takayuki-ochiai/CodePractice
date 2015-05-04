b525b940dd6cd727a0370d23e801c88e444a4d8a
落合隆行 <TakayukiOchiai@ochiaitakayuki-no-MacBook-Pro.local>
2015-05-04 12:56:00 +0900

    サンドイッチメソッドの抽出

---
+++ RubyPractice/Refactoring/extract_surrounding_method.rb
@@ -0,0 +1,27 @@
+class Person
+  attr_reader :mother, :children, :name
+  def initialize(name, date_of_birth, date_of_death = nil, mother = nil)
+    @name, @mother = name, mother
+    @date_of_birth, @date_of_death = date_of_birth, date_of_death
+    @children = []
+    @mother.add_child(self) if @mother
+  end
+
+  def add_child(child)
+    @children << child
+  end
+
+  def number_of_living_decendants
+    children.inject(0) do |count, child|
+      count += 1 if child.alive?
+      count + child.number_of_living_decendants
+    end
+  end
+
+  def number_of_descendants_named(name)
+    children.inject(0) do |count, child|
+      count += 1 if child.name == name
+      count + child.number_of_living_decendants_named(name)
+    end
+  end
+end
