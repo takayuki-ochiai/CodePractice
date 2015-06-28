0fd6d3439ff92595e9f8210cfca5a3dd99b5b6a1
takayuki-ochiai <go.ahead.oti@gmail.com>
2015-06-28 23:18:26 +0900

    htmlを読み込む

--- NodePractice/server.js
+++ NodePractice/server.js
@@ -1,27 +1,26 @@
 //Nodeが持っているhttoモジュールを読み込む
 var http = require('http');
 var settings = require('./settings');
-
+var fs = require('fs');
 
 var server = http.createServer();
 var msg;
 server.on('request', function(req, res){
-    switch(req.url) {
-        case '/about' :
-            msg = 'about this page';
-            break;
-        case '/profile' :
-            msg = 'about me';
-            break;
-        default :
-            msg = 'wrong page';
-            break;
-    }
+    //ファイルを読み込むのは時間がかかるのでコールバック関数を使う
+    fs.readFile(__dirname + '/public_html/index.html', 'utf-8', function(err, data) {
+        if(err) {
+            res.writeHead(404, {'Content-Type': 'text/plain'});
+            res.write('not found');
+            //必ずresponseは終了させること
+            return res.end();
+        }
+        res.writeHead(200, {'Content-Type': 'text/html'});
 
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write(msg);
-    //必ずresponseは終了させること
-    res.end();
+        //読み込まれたhtmlがdata変数に入っている
+        res.write(data);
+        //必ずresponseは終了させること
+        res.end();
+    });
 });
 server.listen(settings.port, settings.host);
 console.log("server listening...");
