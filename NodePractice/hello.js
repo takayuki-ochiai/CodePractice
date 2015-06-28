06c7b012d26dd22ef5274b9b6e1383bd3f320f7e
落合隆行 <TakayukiOchiai@ochiaitakayuki-no-MacBook-Pro.local>
2015-06-28 22:59:34 +0900

    Nodeでサーバーを作る

--- NodePractice/hello.js
+++ NodePractice/hello.js
@@ -1,1 +1,17 @@
-console.log("Hello Node!");
+//console.log("Hello Node!");
+
+// non blocking
+//次の処理 (Worldを出力)を邪魔しないようにコールバック関数で最初の処理を渡す必要がある。
+// setTimeout(function() {
+//     console.log("hello");
+// }, 2000);
+
+// console.log("World");
+
+//逆にブロックしているような処理
+//Nodeはメインのスレッドが一つなので、この書き方をするとすべてのリクエストをブロックするというおそろしい事態になる
+var start = new Date().getTime();
+while(new Date().getTime() < start + 1000);
+console.log("world");
+
+//時間がかかりそうな処理はコールバック関数で実装すること
---
+++ NodePractice/server.js
@@ -0,0 +1,27 @@
+//Nodeが持っているhttoモジュールを読み込む
+var http = require('http');
+var settings = require('./settings');
+
+
+var server = http.createServer();
+var msg;
+server.on('request', function(req, res){
+    switch(req.url) {
+        case '/about' :
+            msg = 'about this page';
+            break;
+        case '/profile' :
+            msg = 'about me';
+            break;
+        default :
+            msg = 'wrong page';
+            break;
+    }
+
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write(msg);
+    //必ずresponseは終了させること
+    res.end();
+});
+server.listen(settings.port, settings.host);
+console.log("server listening...");
