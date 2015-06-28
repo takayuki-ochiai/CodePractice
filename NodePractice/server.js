2b8bc41a89bfbb885bf2d7e2d464f977f9c14af8
takayuki-ochiai <go.ahead.oti@gmail.com>
2015-06-28 23:51:24 +0900

    ejsで埋め込みした

--- NodePractice/server.js
+++ NodePractice/server.js
@@ -2,25 +2,21 @@
 var http = require('http');
 var settings = require('./settings');
 var fs = require('fs');
+var ejs = require('ejs');
 
 var server = http.createServer();
-var msg;
+var template = fs.readFileSync(__dirname + '/public_html/index.ejs', 'utf-8');
+var n = 0;
 server.on('request', function(req, res){
-    //ファイルを読み込むのは時間がかかるのでコールバック関数を使う
-    fs.readFile(__dirname + '/public_html/index.html', 'utf-8', function(err, data) {
-        if(err) {
-            res.writeHead(404, {'Content-Type': 'text/plain'});
-            res.write('not found');
-            //必ずresponseは終了させること
-            return res.end();
-        }
-        res.writeHead(200, {'Content-Type': 'text/html'});
-
-        //読み込まれたhtmlがdata変数に入っている
-        res.write(data);
-        //必ずresponseは終了させること
-        res.end();
+    n++;
+    var data = ejs.render(template, {
+        title: "hello",
+        content: "<strong>World!</strong>",
+        n: n
     });
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
 });
 server.listen(settings.port, settings.host);
 console.log("server listening...");
