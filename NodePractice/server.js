6ae17b0ead57199e1aaca719d4e6d8c32e6eee77
takayuki-ochiai <go.ahead.oti@gmail.com>
2015-06-29 23:56:07 +0900

    nodeで簡単な掲示板投稿機能

--- NodePractice/server.js
+++ NodePractice/server.js
@@ -3,20 +3,50 @@ var http = require('http');
 var settings = require('./settings');
 var fs = require('fs');
 var ejs = require('ejs');
+//投稿を受け取るためのモジュール
+var qs = require('querystring');
 
 var server = http.createServer();
-var template = fs.readFileSync(__dirname + '/public_html/index.ejs', 'utf-8');
+var template = fs.readFileSync(__dirname + '/public_html/bbs.ejs', 'utf-8');
 var n = 0;
-server.on('request', function(req, res){
-    n++;
+
+//投稿を保持しておくための配列
+var posts = [];
+
+function renderForm(posts, res) {
     var data = ejs.render(template, {
-        title: "hello",
-        content: "<strong>World!</strong>",
-        n: n
+        posts: posts
     });
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     res.end();
+}
+server.on('request', function(req, res){
+    if(req.method === 'POST') {
+        //リクエストデータ初期化
+        req.data = "";
+        //読み込み中のイベントはreadableで取れる
+        req.on("readable", function() {
+            req.data += req.read();
+        });
+        //すべてのイベントが終わったらはendで取れる
+        req.on("end", function() {
+            var query = qs.parse(req.data);
+            posts.push(query.name);
+            renderForm(posts, res);
+        });
+    } else {
+        renderForm(posts, res);
+    }
+    // n++;
+    // var data = ejs.render(template, {
+    //     title: "hello",
+    //     content: "<strong>World!</strong>",
+    //     n: n
+    // });
+    // res.writeHead(200, {'Content-Type': 'text/html'});
+    // res.write(data);
+    // res.end();
 });
 server.listen(settings.port, settings.host);
 console.log("server listening...");
