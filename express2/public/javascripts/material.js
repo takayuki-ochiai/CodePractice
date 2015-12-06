91a71bf84ac48322bae206f5e4d9cdbcebfbaf0f
takayuki-ochiai <go.ahead.oti@gmail.com>
2015-12-07 00:59:38 +0900

    いくつかマテリアルデザインを使用

--- express2/public/javascripts/material.js
+++ express2/public/javascripts/material.js
@@ -1,3 +1,9 @@
 angular.module('myApp', ["ngMaterial"])
   .controller('MyController', ['$scope', function($scope) {
+    $scope.users = [
+      { id: 1, name: '東京都' },
+      { id: 2, name: '埼玉県' },
+      { id: 3, name: '神奈川県' }
+    ];
+    $scope.selectedUser = { id: 1, name: '東京都' };
   }]);
